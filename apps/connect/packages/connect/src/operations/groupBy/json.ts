8bda863854c5e96382c611717aacebb23fd0a09c
Liam <liam@ten.dev>
2025-02-08 14:09:38 +0000

    Group By with join on groupKey

--- apps/connect/packages/connect/src/operations/groupBy/json.ts
+++ apps/connect/packages/connect/src/operations/groupBy/json.ts
@@ -1,10 +1,22 @@
 import { type PrismaClient } from "@prisma/client";
 import { type Query } from "~/types/querySchema";
 import { drizzle } from "drizzle-orm/prisma/pg";
-import { count, desc, getTableColumns, max, min, sql, sum } from "drizzle-orm";
+import {
+  count,
+  desc,
+  eq,
+  getTableColumns,
+  max,
+  min,
+  sql,
+  sum,
+  WithSubquery,
+} from "drizzle-orm";
 import { parsePrismaWhere } from "~/util/prismaToDrizzleWhereConditions";
 import assert from "assert";
 import * as drizzleTables from "~/../drizzle/schema";
+import { findRelationsBetweenTables } from "~/util/findRelationsBetweenTables";
+import { AnyPgTable } from "drizzle-orm/pg-core";
 
 const tables: Record<string, any> = drizzleTables;
 
@@ -43,6 +55,48 @@ export async function groupByJson(prisma: PrismaClient, query: Query) {
       .where(drizzleWhere)
   );
 
+  const tableAliasMapper: Record<string, WithSubquery> = {};
+  const tableAliases: WithSubquery[] = [];
+  const tablesToAlias =
+    jsonColumnSchema
+      ?.map((jsonCol) => jsonCol.tableName)
+      .filter((t) => t !== table) || [];
+  for (const table of tablesToAlias) {
+    const jsonSchemaForTable = jsonColumnSchema?.find(
+      (jsonCol) => jsonCol.tableName === table
+    );
+    const jsonCols =
+      jsonSchemaForTable?.jsonSchema.map((col) => col.name) || [];
+    if (jsonCols.length === 0) {
+      continue;
+    }
+    const jsonColumnName = jsonSchemaForTable?.jsonColumnName;
+
+    const tableAlias = db.$with(`${table}Alias`).as(
+      db
+        .select({
+          ...getTableColumns(tables[table]),
+          ...jsonCols.reduce((acc: Record<string, unknown>, col) => {
+            acc[col] = sql
+              .raw(
+                `cast((${jsonColumnName}->>'${col}') as ${
+                  jsonSchemaForTable?.jsonSchema.find(
+                    (jCol) => jCol.name === col
+                  )?.type === "String"
+                    ? "Text"
+                    : "Numeric"
+                })`
+              )
+              .as(col);
+            return acc;
+          }, {}),
+        })
+        .from(tables[table])
+    );
+    tableAliases.push(tableAlias);
+    tableAliasMapper[table] = tableAlias;
+  }
+
   const countJsonFields = operationParameters.count?.columns.map(
     (col) => sql`${col}::text, cast(${count(tableAlias[col])} as int)`
   );
@@ -71,17 +125,43 @@ export async function groupByJson(prisma: PrismaClient, query: Query) {
     selectFields.sum = sql`json_build_object${sumJsonFields}`.as("_sum");
   }
 
-  // TODO: Add support for groupBy after join
-  const response = await db
+  const [[joinTable, joinColumn]] = Object.entries(
+    operationParameters.groupBy[0]?.join ?? {}
+  );
+
+  console.log(joinTable, joinColumn);
+
+  const joinTableAlias: AnyPgTable | WithSubquery =
+    tableAliasMapper[joinTable] || tables[joinTable];
+
+  const dbQuery = db
     .with(tableAlias)
     .select({
       [operationParameters.groupBy[0].column]:
         tableAlias[operationParameters.groupBy[0].column],
       ...selectFields,
+      // @ts-expect-error - We dont know the columns of joinTableAlias
+      ...(joinTable ? { [joinColumn]: joinTableAlias[joinColumn] } : {}),
     })
     .from(tableAlias)
-    .groupBy(tableAlias[operationParameters.groupBy[0].column])
+    .groupBy(
+      tableAlias[operationParameters.groupBy[0].column],
+      // @ts-expect-error - We dont know the columns of joinTableAlias
+      ...(joinTable ? [joinTableAlias[joinColumn]] : [])
+    )
     .orderBy(desc(count(tableAlias["id"])));
 
-  return response;
+  if (joinTable) {
+    const [currentTableKey, relatedTableKey] = findRelationsBetweenTables(
+      tables[table],
+      tables[joinTable]
+    );
+    dbQuery.leftJoin(
+      tables[joinTable],
+      // @ts-expect-error - We dont know the columns of joinTableAlias
+      eq(tableAlias[currentTableKey], joinTableAlias[relatedTableKey])
+    );
+  }
+
+  return dbQuery;
 }

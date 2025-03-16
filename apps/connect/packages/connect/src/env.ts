81439e5c3e969a8013721f6f625eb755175af695
Liam <liam@ten.dev>
2025-03-16 17:52:10 +0000

    Remove need for Inconvo API key in createEnv

--- apps/connect/packages/connect/src/env.ts
+++ apps/connect/packages/connect/src/env.ts
@@ -6,7 +6,6 @@ export const env = createEnv({
   server: {
     INCONVO_DATABASE_URL: z.string(),
     INCONVO_SECRET_KEY: z.string().min(1),
-    INCONVO_API_KEY: z.string().min(1),
     NODE_ENV: z.enum(["development", "production", "test"]),
     DRIZZLE: z.enum(["TRUE", "FALSE"]).default("FALSE"),
   },

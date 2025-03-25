32db8d3ad88cb64c76dee72f48baee77ef0dde3a
Liam <liam@ten.dev>
2025-03-25 11:00:36 +0000

    Update schema.ts to clarify push restrictions and maintain file integrity

--- apps/connect/packages/connect/drizzle/schema.ts
+++ apps/connect/packages/connect/drizzle/schema.ts
@@ -1,2 +1,2 @@
-// Do  not push any changes to this file on git.	import { pgTable, foreignKey, text, integer, timestamp, doublePrecision, numeric, boolean } from "drizzle-orm/pg-core"
+// Do  not push any changes to this file on git.
 export const empty = {};

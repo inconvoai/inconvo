ef949c814f9a2611d0993ee07cc6bdded63194cc
tendev-eoghan <111668850+tendev-eoghan@users.noreply.github.com>
2025-08-16 20:24:31 +0100

    refactor: change error logging level to debug in compileSchemas function

--- apps/connect/packages/connect/src/pullSchema.js
+++ apps/connect/packages/connect/src/pullSchema.js
@@ -184,7 +184,7 @@ function compileSchemas(drizzlePath) {
   } catch (error) {
     // Error has already been logged by runDrizzleCommand
     logger.error("An error occurred during schema pull and compilation.");
-    logger.info(error);
+    logger.debug(error);
     process.exit(1);
   }
 })();
